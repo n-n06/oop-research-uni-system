eaeac2be7ded358f78a60c24e3751fac7d8109e9
e1kan <156985217+e1kan@users.noreply.github.com>
2024-12-09 15:09:29 +0500

    Update StudentAlphabetComparator.java

--- ResearchUniversitySystem/Model/utilities/comparators/StudentAlphabetComparator.java
+++ ResearchUniversitySystem/Model/utilities/comparators/StudentAlphabetComparator.java
@@ -1,28 +1,26 @@
 package utilities.comparators;
 
 import java.util.Comparator;
-
 import users.students.Student;
 
 /**
- * 
+ * Comparator to sort Student objects alphabetically by their names
  */
+
 public class StudentAlphabetComparator extends StudentsComparator {
 
     /**
-     * Default constructor
+     * Compares two Student objects alphabetically by their names
+     *
+     * @param s1 - the first student
+     * @param s2 - the second student
+     * @return a negative integer, zero, or a positive integer as the name of s1
+     *         is lexicographically less than, equal to, or greater than the name of s2
      */
-    public StudentAlphabetComparator() {
-    }
-
-    /**
-     * @param Student s1 
-     * @param Student s2 
-     * @return
-     */
+	
+    @Override
     public int compare(Student s1, Student s2) {
-        // TODO implement here
-        return 0;
+        return s1.getName().compareTo(s2.getName()); // sorting in asccending order: 'Alice', 'Bob', ...
     }
-
+    
 }
