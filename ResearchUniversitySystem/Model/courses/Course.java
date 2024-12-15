c6c74f8fed4b359e82c8df717cca1c96f6f7de44
Muslik <ahmetaliev345@gmail.com>
2024-12-15 22:58:29 +0500

    refactor: Course class

--- ResearchUniversitySystem/Model/courses/Course.java
+++ ResearchUniversitySystem/Model/courses/Course.java
@@ -6,130 +6,126 @@ import java.util.HashMap;
 import java.io.Serializable;
 
 import enums.CourseType;
+import enums.LessonType;
 import enums.Semester;
 import users.students.Student;
 import users.employees.Teacher;
 
 /**
  * @author eva
+ * @author Muslik
  */
 
-// problem: cant have duplicate subjects (with the same id) but can have courses that belong to one subjects
-// add credit system (2/0/1 - 2 lectures, 0 labs, 1 practice)
 // add formula for calculating how many lessons are being held - add a system to calculate lessons
-public class Course extends Subject implements Serializable {
+
+public class Course implements Serializable {
+	
 	private String courseID;
-	private static HashMap<String, String> courses = new HashMap<>(); 
-    /**
-     * Default constructor
-     */
-    public Course(String subjectID, String subjectName, int courseID) {
-    	super(subjectID, subjectName);
-    	this.courseID = subjectID + courseID;
-    	addCourse(this);
-    }
-/*
-    /**
-     * 
-     */
+	private String courseName;
     private String description;
-
-    /**
-     * 
-     */
-    private int credits;
-
-    /**
-     * 
-     */
-    private HashSet<Course> prerequesites;
-
-    /**
-     * 
-     */
-    private Vector<Lesson> courseLessons;
-
-    /**
-     * 
-     */
-    private Vector<Teacher> courseTeachers;
-
-    /**
-     * 
-     */
+    private HashMap<LessonType, Integer> credits = new HashMap<>();   // its like (2/0/1 - 2 lectures, 0 labs, 1 practice)
+    private HashSet<Course> prerequesites = new HashSet<>();
+    private Vector<Lesson> courseLessons = new Vector<>();
+    private Vector<Teacher> courseTeachers = new Vector<>();
     private Semester semester;
-
-    /**
-     * 
-     */
     private CourseType courseType;
-
-    /**
-     * 
-     */
+    
     private HashMap<Student,Mark> gradeBook;
 
-    /**
-     * @param student 
-     * @return
-     */
+    public Course(String courseID, String courseName) {
+    	this.courseID = courseID;
+    	this.courseName = courseName;
+    }
+    
+    public Course(String courseID, String courseName, HashSet<Course> prerequesites) {
+    	this.courseID = courseID;
+    	this.courseName = courseName;
+    	this.prerequesites = prerequesites;
+    }
+    
     public boolean checkPrerequesites(Student student) {
-        // TODO implement here
-        return false;
+        for (Course c : prerequesites) {
+        	if (!student.getCompletedCourses().contains(c)) {
+        		return false;
+        	}
+        }
+        return true;
+    }
+    
+    public String getID() {
+    	return courseID;
     }
 
-    /**
-     * @param lesson 
-     * @return
-     */
     public void addLesson(Lesson lesson) {
         // TODO implement here
         return;
     }
 
-    /**
-     * @param lesson 
-     * @return
-     */
     public void removeLesson(Lesson lesson) {
         // TODO implement here
         return;
     }
 
-    /**
-     * @return
-     */
     public HashMap<Student,Mark> getGradeBook() {
         // TODO implement here
         return null;
     }
 
-    /**
-     * @return
-     */
     public GradeReport getGradeReport() {
         // TODO implement here
         return null;
     }
 
-    /**
-     * @param cr 
-     * @param student 
-     * @return
-     */
+    public void addTeacherToCourse(Teacher teacher) {
+    	courseTeachers.add(teacher);
+    }
+
     public void assignRetake(CourseRepository cr, Student student) {
         // TODO implement here
         return;
     }
 
-    /**
-     * @param student 
-     * @param mark 
-     * @return
-     */
     public void assignMark(Student student, Mark mark) {
         // TODO implement here
         return;
     }
-*/
+    
+    //Setters
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPrerequesites(HashSet<Course> prerequesites) {
+        this.prerequesites = prerequesites;
+    }
+
+    public void setCourseLessons(Vector<Lesson> courseLessons) {
+        this.courseLessons = courseLessons;
+    }
+
+    public void setCourseTeachers(Vector<Teacher> courseTeachers) {
+        this.courseTeachers = courseTeachers;
+    }
+
+    public void setCredits(LessonType type, int creditValue) {
+        credits.put(type, creditValue);
+    }
+    
+    public void setSemester(Semester semester) {
+        this.semester = semester;
+    }
+
+    public void setCourseType(CourseType courseType) {
+        this.courseType = courseType;
+    }
+
+    public void setGradeBook(HashMap<Student, Mark> gradeBook) {
+        this.gradeBook = gradeBook;
+    }
+
+  
+    public String toString() {
+    	return "CourseID : " + courseID + " || courseName: " + courseName;
+    }
+
 }
