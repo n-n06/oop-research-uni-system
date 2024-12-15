cce985127020144b36c91afd7c572a342cc21d4f
Muslik <ahmetaliev345@gmail.com>
2024-12-15 23:04:31 +0500

    add constructor to bachelor student

--- ResearchUniversitySystem/Model/users/students/BachelorStudent.java
+++ ResearchUniversitySystem/Model/users/students/BachelorStudent.java
@@ -4,16 +4,19 @@ package users.students;
  * 
  */
 public class BachelorStudent extends Student {
+	 public int year;
 
-    /**
-     * Default constructor
-     */
-    public BachelorStudent() {
+ 
+    public BachelorStudent(String firstName, String lastName, String email, int year) {
+    	super(firstName, lastName, email);
+    	this.year = year;
+    }
+    
+    @Override
+    public boolean login(String email, String password, UserRepository userRepo) {
+    	super.login(getUserEmail(), getPassword(), userRepo);
     }
 
-    /**
-     * 
-     */
-    public int year;
-
+  
+   
 }
